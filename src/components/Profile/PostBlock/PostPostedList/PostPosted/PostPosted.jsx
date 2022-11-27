543576323aeb8013a050a17cad1fcdad6daa686f
Rustam N <Rus89.92@mail.ru>
2022-11-28 00:08:43 +0500

    changed grid to flex in PostedPost

--- src/components/Profile/PostBlock/PostPostedList/PostPosted/PostPosted.jsx
+++ src/components/Profile/PostBlock/PostPostedList/PostPosted/PostPosted.jsx
@@ -5,19 +5,23 @@ import s from './PostPosted.module.css';
 function PostPosted(props) {
   const photo = usersInfo.list[props.id].photo;
   const message = props.message;
-  return ( 
+  return (
     <div className={s["post-posted"]}>
       <div className={s["post-posted__user-photo"]}>
         <img src={photo} alt="post-posted__user-photo" />
       </div>
-      <div className={s["post-posted__message"]}>
-        {`
-        agemessagemessagemessagemessagemessagemessagemessagemesagemessagemessagemessagemessagemessagemessagemessagemesagemessagemessagemessagemessagemessagemessagemessagemesagemessagemessagemessagemessagemessagemessagemessagemesagemessagemessagemessagemessagemessagemessagemessagemes
-        `}
-      </div>
-      <div className={s["post-posted__rating-buttons"]}>
-        <button className={s['btn-like']}>Like</button>
-        <button className={s['btn-dislike']}>Dislike</button>
+      <div className={s["post-posted__msg-and-btns-cont"]}>
+        <div className={s["post-posted__message"]}>
+          <p>
+            {`
+            agemessagemessagemessageagemessagemessagemessageagemessagemessagemessageagemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage agemessagemessagemessage
+            `}
+          </p>
+        </div>
+        <div className={s["post-posted__rating-buttons"]}>
+          <button className={s['btn-like']}>Like</button>
+          <button className={s['btn-dislike']}>Dislike</button>
+        </div>
       </div>
     </div>
   );
