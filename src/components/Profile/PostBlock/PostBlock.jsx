c1da8a32a61e76a67357be268629d477981b74a0
Rustam N <Rus89.92@mail.ru>
2022-12-02 20:52:06 +0500

    Changed render method of PostsList

--- src/components/Profile/PostBlock/PostBlock.jsx
+++ src/components/Profile/PostBlock/PostBlock.jsx
@@ -2,22 +2,32 @@ import { React, useState } from 'react';
 import s from './PostBlock.module.css';
 import PostPostedList from './PostPostedList/PostPostedList';
 import PostPoster from './PostPoster/PostPoster';
+import PostPosted from './PostPostedList/PostPosted/PostPosted';
+ 
 
 function PostBlock({ state, actions }) {
-  const { usersPostPosterText, currentUserID } = state;
+  const { usersPostPosterText, currentUserID, usersPosts, usersProfileInfo } = state;
   const stateTo_PostPoster = { usersPostPosterText, currentUserID };
 
-  const [trigger, setTrigger] = useState(false);
+  const stateOfPostsList = () => (usersPosts.list[currentUserID.id]
+    .map((post) => {
+      return (<PostPosted
+        key={post.messageID}
+        post={post}
+        photo={usersProfileInfo.list[post.userID].photo}
+      />)
+    }));
+
+  const [postedPosts, setPostedPosts] = useState(stateOfPostsList());
   const addPost = (message) => {
     actions.createPost(message);
-    actions.rerenderApp(); // для рендера PostPostedList через рендер всего root
-    // setTrigger((prev) => !prev);  // для реренедера PostPostedList через триггерный useState
+    setPostedPosts(stateOfPostsList());
   };
   return (
     <div className={s.postBlock}>
       <h1 className={s.postBlock__title}>My posts</h1>
-      <PostPoster addPost={addPost} state={stateTo_PostPoster} actions={actions} />
-      <PostPostedList state={state} trigger={trigger} />
+      <PostPoster addPost={addPost} state={stateTo_PostPoster} />
+      <PostPostedList state={state} postedPosts={postedPosts} />
     </div>
   );
 }
