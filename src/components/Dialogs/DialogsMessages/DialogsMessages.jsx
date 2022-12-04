d4470bf603093d4e7259f8a83205593288d95f9d
Rustam N <Rus89.92@mail.ru>
2022-12-04 21:36:22 +0500

    test

--- src/components/Dialogs/DialogsMessages/DialogsMessages.jsx
+++ src/components/Dialogs/DialogsMessages/DialogsMessages.jsx
@@ -1,18 +1,24 @@
-import { React } from 'react';
+import { React, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import s from './DialogsMessages.module.css';
 
 import DialogsText from './DialogsText/DialogsText';
 import DialogsMessageSender from './DialogsMessageSender/DialogsMessageSender';
+import { getMessageSenderTextActionCreator, sendMessageSenderTextActionCreator, setMessageSenderTextActionCreator } from '../../../dataBase/state';
 
+export function DialogsMessages({ store }) {
+  const {
+    userMessages,
+    currentUserID,
+    usersProfileInfo,
+  } = store.state;
 
-function DialogsMessages({ store }) {
-  const { userMessages, currentUserID, usersProfileInfo } = store.state;
   const { userID } = useParams();
 
-  let key = 1;
-  const arrayOfDialogText = userMessages.list[userID]
-    .map(({ me, message }) => {
+  const stateOfDialogsMessages = () => {
+    let key = 1;
+    console.log(userID);
+    return userMessages.list[currentUserID.id].list[userID].map(({ me, message }) => {
       const messageAttributes = {
         my: me,
         message: message,
@@ -20,14 +26,32 @@ function DialogsMessages({ store }) {
       };
       return <DialogsText key={key++} messageAttributes={messageAttributes} />
     });
+  }
+  const [arrayOfDialogText, setArrayOfDialogText] = useState(stateOfDialogsMessages()); //!!!
+
+  const getTextFromBLL = () => {
+    store.dispatch(getMessageSenderTextActionCreator());
+  }
+  const setTextToBLL = (currentTextUI) => {
+    store.dispatch(setMessageSenderTextActionCreator(currentTextUI));
+  }
+  const sendText = () => {
+    store.dispatch(sendMessageSenderTextActionCreator(userID));
+    setArrayOfDialogText(stateOfDialogsMessages());
+    // store.rerender();
+    // setArrayOfDialogText((prev) => prev + 1); //!!!
+  };
+
   return (
     <div className={s.DialogsMessages}>
       <div className={s.DialogsMessages__container + " scrollBar"}>
         {arrayOfDialogText}
       </div>
-      <DialogsMessageSender />
+      <DialogsMessageSender
+        getTextFromBLL={getTextFromBLL}
+        setTextToBLL={setTextToBLL}
+        sendText={sendText}
+      />
     </div >
   );
 }
-
-export { DialogsMessages };
